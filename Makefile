1d6c579482154ba6a4a45baf046dfa18054a664c
MasahiroKiji <mkiji@coe.edu>
2019-02-07 14:21:05 -0600

    Update makefile and calc for division

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 .PHONY: all
 all: calc
 
-calc: calc.o add.o sub.o mul.o add.h sub.h mul.h
-	gcc -o calc calc.o add.o sub.o mul.o
+calc: calc.o add.o sub.o mul.o div.o add.h sub.h mul.h div.h
+	gcc -o calc calc.o add.o sub.o mul.o div.o
 
-calc.o: calc.c add.h sub.h mul.h
+calc.o: calc.c add.h sub.h mul.h div.h
 	gcc -c calc.c
 
 add.o: add.c
@@ -15,6 +15,9 @@ sub.o: sub.c
 
 mul.o: mul.c
 		gcc -c mul.c
+
+div.o: div.c
+		gcc -c div.c
 
 .PHONY: clean
 clean:
--- calc.c
+++ calc.c
@@ -3,13 +3,17 @@
 #include "add.h"
 #include "sub.h"
 #include "mul.h"
+#include "div.h"
 
 void main(int argc, char *argv[]) {
 
+  int array_for_division[3];
+  divi(atoi(argv[1]), atoi(argv[2]), array_for_division);
   int sum = add( atoi(argv[1]), atoi(argv[2]));
   int dif = sub( atoi(argv[1]), atoi(argv[2]));
   int pro = mul( atoi(argv[1]), atoi(argv[2]));
   printf("%d\n", sum);
   printf("%d\n", dif);
   printf("%d\n", pro);
+  printf("%d + %d/%d\n", array_for_division[0], array_for_division[1], array_for_division[2]);
 }
